a0136bd76299914d3a2f64210baa9489cc3e07a2
Emma <emmalivne.03@gmail.com>
2023-05-16 18:19:13 +0300

    Add IntakeIO object to Intake

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.intake;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
+    private final IntakeIO intakeIO;
 
     public void setAngleMotorVelocity(double velocity){
         intakeIO.setAngleMotorVelocity(velocity);
