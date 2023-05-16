d89bea5643aaa9fa3b8246cecad1c0258fcb4266
Emma <emmalivne.03@gmail.com>
2023-05-16 18:17:27 +0300

    Create IntakeIO interface

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+    default void updateInputs(IntakeInputs inputs){
+    }
+
+    default void setAngleMotorVelocity(double velocity){
+    }
+
+    default void setSpinMotorVelocity(double velocity){
+    }
+
+    default void setAngle(double angle){
+    }
+
+    @AutoLog
+    class IntakeInputs{
+        public double angle = 0;
+        public double angleSetpoint = 0;
+        public double angleMotorVelocity = 0;
+        public double angleMotorVelocitySetpoint = 0;
+        public double motorVelocity = 0;
+        public double motorVelocitySetpoint = 0;
+
+    }
+}
