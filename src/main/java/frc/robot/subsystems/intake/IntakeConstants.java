1afdc95baaa9dfbdbf84450a2f2e32b275c57202
Emma <emmalivne.03@gmail.com>
2023-05-16 18:17:15 +0300

    Create Intake Constants

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.intake;
+
+public class IntakeConstants {
+    public static final double GEARING = 0;
+    public static final double ANGLE_MOTOR_MOMENT_OF_INERTIA = 0;
+    public static final double SPIN_MOTOR_MOMENT_OF_INERTIA = 0;
+}
