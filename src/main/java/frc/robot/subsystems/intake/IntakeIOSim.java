8daf3cae70590ce77e0c74497870b9f3dc47382e
Emma <emmalivne.03@gmail.com>
2023-05-17 13:01:51 +0300

    Change spinMotor to NEO

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -2,15 +2,18 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import frc.robot.utils.math.differential.Integral;
 
 public class IntakeIOSim implements IntakeIO{
 
     private final FlywheelSim spinMotor;
     private final FlywheelSim angleMotor;
 
+    private final Integral angle = new Integral(0, 0);
+
     public IntakeIOSim() {
         this.spinMotor = new FlywheelSim(
-                DCMotor.getFalcon500(1), 1/IntakeConstants.GEARING, IntakeConstants.SPIN_MOTOR_MOMENT_OF_INERTIA);
+                DCMotor.getNEO(1), 1/IntakeConstants.GEARING, IntakeConstants.SPIN_MOTOR_MOMENT_OF_INERTIA);
         this.angleMotor = new FlywheelSim(
                 DCMotor.getFalcon500(1), 1/IntakeConstants.GEARING, IntakeConstants.ANGLE_MOTOR_MOMENT_OF_INERTIA
         );
@@ -23,16 +26,15 @@ public class IntakeIOSim implements IntakeIO{
 
         inputs.angleMotorVelocity = angleMotor.getAngularVelocityRadPerSec();
         inputs.spinMotorVelocity = spinMotor.getAngularVelocityRadPerSec();
+        angle.update(angleMotor.getAngularVelocityRadPerSec());
     }
 
     @Override
     public void setAngleMotorVelocity(double velocity) {
-        IntakeIO.super.setAngleMotorVelocity(velocity);
     }
 
     @Override
     public void setSpinMotorVelocity(double velocity) {
-        IntakeIO.super.setSpinMotorVelocity(velocity);
     }
 
     @Override
