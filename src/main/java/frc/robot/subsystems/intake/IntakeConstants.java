1a84a518371afa4f737ca08a21fa6a54b682acf7
Emma <emmalivne.03@gmail.com>
2023-05-19 13:53:44 +0300

    Add Intake constants

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -4,4 +4,7 @@ public class IntakeConstants {
     public static final double GEARING = 0;
     public static final double ANGLE_MOTOR_MOMENT_OF_INERTIA = 0;
     public static final double SPIN_MOTOR_MOMENT_OF_INERTIA = 0;
+    public static final double VOLT_COMPENSATION = 12;
+    public static final double FALCON_TICKS_PER_ROTATION = 2048;
+    public static final double TICKS_PER_DEGREE = FALCON_TICKS_PER_ROTATION/ 360*GEARING;
 }
