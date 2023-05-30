edb984b95ee14990d009825beba26abb91158e3d
Emma <emmalivne.03@gmail.com>
2023-05-30 20:14:38 +0300

    Add most constants

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -1,10 +1,11 @@
 package frc.robot.subsystems.intake;
 
 public class IntakeConstants {
-    public static final double GEARING = 0;
+    public static final double ANGLE_MOTOR_GEARING = 35.26;
+    public static final double SPIN_MOTOR_GEARING = 0;
     public static final double ANGLE_MOTOR_MOMENT_OF_INERTIA = 0;
     public static final double SPIN_MOTOR_MOMENT_OF_INERTIA = 0;
     public static final double VOLT_COMPENSATION = 12;
-    public static final double FALCON_TICKS_PER_ROTATION = 2048;
-    public static final double TICKS_PER_DEGREE = FALCON_TICKS_PER_ROTATION/ 360*GEARING;
+    public static final double FALCON_TICKS_PER_ROTATION = 2048 * ANGLE_MOTOR_GEARING;
+    public static final double ANGLE_MOTOR_TICKS_PER_RADIAN = FALCON_TICKS_PER_ROTATION/ (Math.PI*2);
 }
