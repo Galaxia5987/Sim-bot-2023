0ac4ebf1fed861c3cd3d92deefc6093d477f1165
Emma <emmalivne.03@gmail.com>
2023-05-30 20:15:10 +0300

    Add constructor and periodic

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -5,6 +5,12 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Intake extends SubsystemBase {
     private final IntakeIO intakeIO;
 
+    private IntakeIO.IntakeInputs inputs;
+
+    public Intake (IntakeIO intakeIO){
+        this.intakeIO = intakeIO;
+    }
+
     public void setAngleMotorPower(double power){
         intakeIO.setAngleMotorPower(power);
     }
@@ -16,4 +22,11 @@ public class Intake extends SubsystemBase {
     public void setSpinMotorPower(double power){
         intakeIO.setSpinMotorPower(power);
     }
+
+    @Override
+    public void periodic() {
+        intakeIO.updateInputs(inputs);
+    }
+
+
 }
