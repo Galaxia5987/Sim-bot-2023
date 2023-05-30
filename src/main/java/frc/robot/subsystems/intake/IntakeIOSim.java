b5232aeb7e66215465f3428446dd83ad601f2349
Emma <emmalivne.03@gmail.com>
2023-05-30 20:13:02 +0300

    Fix setAngle function and some more

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import frc.robot.utils.math.differential.Integral;
 
@@ -9,14 +11,19 @@ public class IntakeIOSim implements IntakeIO{
     private final FlywheelSim spinMotor;
     private final FlywheelSim angleMotor;
 
+    private final PIDController angleFeedback;
+    private double angleVoltage = 0;
+    private double currentAngle = 0;
+
     private final Integral angle = new Integral(0, 0);
 
     public IntakeIOSim() {
         this.spinMotor = new FlywheelSim(
-                DCMotor.getNEO(1), 1/IntakeConstants.GEARING, IntakeConstants.SPIN_MOTOR_MOMENT_OF_INERTIA);
+                DCMotor.getNEO(1), 1/IntakeConstants.SPIN_MOTOR_GEARING, IntakeConstants.SPIN_MOTOR_MOMENT_OF_INERTIA);
         this.angleMotor = new FlywheelSim(
-                DCMotor.getFalcon500(1), 1/IntakeConstants.GEARING, IntakeConstants.ANGLE_MOTOR_MOMENT_OF_INERTIA
+                DCMotor.getFalcon500(1), 1/IntakeConstants.ANGLE_MOTOR_GEARING, IntakeConstants.ANGLE_MOTOR_MOMENT_OF_INERTIA
         );
+        this.angleFeedback = new PIDController(0, 0, 0);
     }
 
     @Override
@@ -24,22 +31,27 @@ public class IntakeIOSim implements IntakeIO{
         spinMotor.update(0.02);
         angleMotor.update(0.02);
 
+        angle.update(angleMotor.getAngularVelocityRadPerSec());
+        inputs.angle = angle.get();
+        currentAngle = angle.get();
         inputs.angleMotorVelocity = angleMotor.getAngularVelocityRadPerSec();
         inputs.spinMotorVelocity = spinMotor.getAngularVelocityRadPerSec();
-        angle.update(angleMotor.getAngularVelocityRadPerSec());
+        inputs.angleVoltage = angleVoltage;
     }
 
     @Override
-    public void setAngleMotorPower(double velocity) {
-
+    public void setAngleMotorPower(double volts) {
+        angleMotor.setInputVoltage(volts);
     }
 
     @Override
-    public void setSpinMotorPower(double power) {
+    public void setSpinMotorPower(double volts) {
+        spinMotor.setInputVoltage(volts);
     }
 
     @Override
     public void setAngle(double angle) {
-        IntakeIO.super.setAngle(angle);
+        angleVoltage = angleFeedback.calculate(currentAngle, angle);
+        angleMotor.setInputVoltage(angleVoltage);
     }
 }
