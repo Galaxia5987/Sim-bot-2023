fd1127b84b5bcbffe3ba940ed9aaea0bc7d04c17
GaiaZ <gaya.zano11@gmail.com>
2023-05-31 13:37:28 +0300

    Add functions to ShooterIOReal

--- src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOReal.java
@@ -1,37 +1,70 @@
 package frc.robot.subsystems.shooter;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Ports;
+import frc.robot.utils.Utils;
+import frc.robot.utils.motors.PIDTalon;
+
 import frc.robot.utils.units.UnitModel;
 
-public class ShooterIOReal extends SubsystemBase {
+import static frc.robot.Constants.TALON_TIMEOUT;
+
+public class ShooterIOReal extends SubsystemBase implements ShooterIO{
 
     public static ShooterIOReal INSTANCE;
 
-    private final WPI_TalonFX mainMotor = new WPI_TalonFX(0);
-    private final WPI_TalonFX auxMotor = new WPI_TalonFX(0);
+    private final WPI_TalonFX motor = new WPI_TalonFX(Ports.Shooter.MAIN_MOTOR);
 
-    private final UnitModel unitModel = new UnitModel(0);
+    private final UnitModel unitModel = new UnitModel(ShooterConstants.TICKS_PER_ROTATION);
 
+    private double setpoint = 0;
 
-    public ShooterIOReal(){
-        PIDController pidController = new PIDController(0,0,0);
+    public ShooterIOReal() {
+        motor.configFactoryDefault(TALON_TIMEOUT);
+        motor.setInverted(TalonFXInvertType.Clockwise);
+        motor.setNeutralMode(NeutralMode.Coast);
+        motor.config_kP(0, ShooterConstants.PID_CONSTANTS.kP);
+        motor.config_kI(0, ShooterConstants.PID_CONSTANTS.kI);
+        motor.config_kD(0, ShooterConstants.PID_CONSTANTS.kD);
+        motor.config_kF(0, ShooterConstants.Kf);
     }
 
-    public static ShooterIOReal getInstance() {
-        if (INSTANCE == null) {
-            INSTANCE = new ShooterIOReal();
+        public static ShooterIOReal getInstance() {
+            if (INSTANCE == null) {
+                INSTANCE = new ShooterIOReal();
+            }
+            return INSTANCE;
         }
-        return INSTANCE;
+
+    @Override
+    public void updateInput(ShooterInputs inputs) {}
+
+    @Override
+    public void setVelocity(double velocity) {
+        motor.set(ControlMode.Velocity, unitModel.toTicks100ms(velocity / 60));
+        setpoint = velocity;
     }
 
-    private void setVelocity(){
-
+    @Override
+    public double getVelocity() {
+        return unitModel.toVelocity(motor.getSelectedSensorVelocity()) * 60;
     }
 
-    private int getVelocity(){
-        unitModel.toVelocity(mainMotor.getSelectedSensorVelocity());
+    @Override
+    public double getSetpoint() {
+        return setpoint;
+    }
+
+    @Override
+    public boolean atSetPoint(double tolerance) {
+        return Utils.deadband(getVelocity() - setpoint, tolerance) == 0;
     }
 
 }
