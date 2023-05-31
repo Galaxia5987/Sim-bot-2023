bca358258a0e618659c40a756400262b330724b6
GaiaZ <gaya.zano11@gmail.com>
2023-05-31 13:37:56 +0300

    Add Constants

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,4 +1,12 @@
 package frc.robot.subsystems.shooter;
 
+import com.pathplanner.lib.auto.PIDConstants;
+
 public class ShooterConstants {
+    public static final PIDConstants PID_CONSTANTS = new PIDConstants(1, 0, 0);
+    public static final double Kf = 0;
+    public static final double SHOOTER_VELOCITY_DEADBAND = 50;
+    public static final int TICKS_PER_ROTATION = 2048;
+    public static final double MAX_WARMUP_VELOCITY = 4600;
+    public static final double MOMENT_OF_INERTIA = 0.0006;
 }
