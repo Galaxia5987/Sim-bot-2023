cd16a2967e2dfa8eff3ed218f1ccb8fea71e25ff
Emma <emmalivne.03@gmail.com>
2023-06-16 13:47:18 +0300

    Remove unnecessary things from robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,13 +9,10 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.drivetrain.Drive;
-import frc.robot.subsystems.drivetrain.commands.XboxDrive;
 
 public class RobotContainer {
     private static RobotContainer INSTANCE = null;
 
-    private final Drive drive = Drive.getInstance();
 
     private final XboxController xboxController = new XboxController(0);
 
@@ -35,7 +32,6 @@ public class RobotContainer {
     }
 
     private void configureDefaultCommands() {
-        drive.setDefaultCommand(new XboxDrive(xboxController));
     }
 
     private void configureButtonBindings() {
@@ -48,17 +44,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return new InstantCommand(() -> drive.resetOdometry(new Pose2d(new Translation2d(1.93, 4.96), new Rotation2d()), 0))
-                .andThen(new PPSwerveControllerCommand(
-                        PathPlanner.loadPath("Test", 5, 3),
-                        drive::getCurrentPose,
-                        new PIDController(7, 0, 0),
-                        new PIDController(7, 0, 0),
-                        new PIDController(10, 0, 0),
-                        (speeds) -> drive.drive(speeds, false),
-                        false,
-                        drive
-                ));
+        return null;
     }
-
 }
