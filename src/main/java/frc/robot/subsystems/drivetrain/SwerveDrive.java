00ae13994be177f19219822675b061036f99a20d
Emma <emmalivne.03@gmail.com>
2023-06-20 12:55:23 +0300

    Add drive function

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveDrive.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.drivetrain;
 
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,15 +16,36 @@ public class SwerveDrive extends SubsystemBase {
             SwerveConstants.wheelPositions[2],
             SwerveConstants.wheelPositions[3]);
 
-    public SwerveDrive(int[] driveMotorPorts, int[] angleMotorPorts, int[] encoderPorts){
-        for (int i=0; i< modules.length; i++){
+    public SwerveDrive(int[] driveMotorPorts, int[] angleMotorPorts, int[] encoderPorts) {
+        for (int i = 0; i < modules.length; i++) {
             modules[i] = new SwerveModule(driveMotorPorts[i], angleMotorPorts[i], encoderPorts[i]);
         }
     }
 
-    public void setModuleStates(SwerveModuleState[] desiredModuleStates){
-        for (int i=0; i< modules.length; i++){
+    public void setModuleStates(SwerveModuleState[] desiredModuleStates) {
+        for (int i = 0; i < modules.length; i++) {
             modules[i].setModuleState(desiredModuleStates[i]);
         }
+    }
+
+    public void drive(ChassisSpeeds chassisSpeeds) {
+        setModuleStates(kinematics.toSwerveModuleStates(chassisSpeeds));
+    }
+
+    /**
+     * Sets the desired percentage of x, y and omega speeds for the swerve
+     *
+     * @param xOutput     percentage of the x speed
+     * @param yOutput     percentage of the y speed
+     * @param omegaOutput percentage of the omega speed
+     */
+    public void drive(double xOutput, double yOutput, double omegaOutput) {
+
+        ChassisSpeeds chassisSpeeds = new ChassisSpeeds(
+                SwerveConstants.MAX_X_VELOCITY * xOutput,
+                SwerveConstants.MAX_Y_VELOCITY * yOutput,
+                SwerveConstants.MAX_OMEGA_VELOCITY * omegaOutput);
+
+        setModuleStates(kinematics.toSwerveModuleStates(chassisSpeeds));
     }
 }
