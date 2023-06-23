8a0dfe677d6777e2b180bfdff62108f011a139dc
Emma <emmalivne.03@gmail.com>
2023-06-23 14:03:01 +0300

    Fox xboxDrive

--- src/main/java/frc/robot/subsystems/drivetrain/command/XboxDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/command/XboxDrive.java
@@ -19,8 +19,8 @@ public class XboxDrive extends CommandBase {
     @Override
     public void execute() {
         swerveDrive.drive(
-                -MathUtil.applyDeadband(xboxController.getLeftX(), SwerveConstants.XBOX_DEADBAND),
                 -MathUtil.applyDeadband(xboxController.getLeftY(), SwerveConstants.XBOX_DEADBAND),
+                -MathUtil.applyDeadband(xboxController.getLeftX(), SwerveConstants.XBOX_DEADBAND),
                 -MathUtil.applyDeadband(xboxController.getRightX(), SwerveConstants.XBOX_DEADBAND)
         );
     }
