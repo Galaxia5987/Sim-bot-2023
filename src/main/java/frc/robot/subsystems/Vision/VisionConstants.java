1d44e492c9be987ad5d942ab36237cb26280737f
Alon Schwierz <@1>
2023-06-30 12:57:50 +0300

    Add TargetPoseFieldOriented

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.Vision;
 
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 
 public class VisionConstants{
@@ -7,4 +8,5 @@ public class VisionConstants{
     public static final double TARGET_HEIGHT_FROM_GROUND = 0;
     public static final double CAMERA_PITCH = 0;
     public static final Transform3d ROBOT_TO_CAM = null;
+    public static final Pose3d TARGET_POSITION = new Pose3d();
 }
