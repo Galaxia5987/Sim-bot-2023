364f6ba81d24c132fe5289a24dd5cae59ec35dde
Emma <emmalivne.03@gmail.com>
2023-07-02 18:08:33 +0300

    Add getModulePosition function

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveModule.java
@@ -4,6 +4,7 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -99,6 +100,17 @@ public class SwerveModule extends SubsystemBase {
         return new SwerveModuleState(
                 ticksPerMeter.toUnits(driveMotor.getSelectedSensorVelocity()),
                 new Rotation2d(ticksPerRad.toUnits(angleMotor.getSelectedSensorPosition()))
+        );
+    }
+
+    /**
+     * Gets the position of the module.
+     * @return Position of the module.
+     */
+    public SwerveModulePosition getModulePosition(){
+        return new SwerveModulePosition(
+                ticksPerMeter.toUnits(driveMotor.getSelectedSensorPosition()),
+                new Rotation2d(getAngle())
         );
     }
 
