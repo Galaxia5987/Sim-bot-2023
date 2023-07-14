a83f6c749e18f8a891a98a103f3529b1392132d1
Alon Schwierz <@1>
2023-07-14 11:20:34 +0300

    aprilChooser done

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -8,28 +8,8 @@ public class VisionConstants {
     public static final double TARGET_HEIGHT_FROM_GROUND = 0;
     public static final double CAMERA_PITCH = 0;
     public static final Transform3d ROBOT_TO_CAM = null;
-    public static final Pose3d TARGET_POSITION = new Pose3d();
+    public static Pose3d TARGET_POSITION = new Pose3d();
 
-    public void aprilChooser(int ip) {
-        switch (ip) {
-            case 1:
-                new Pose3d();
-                break;
-            case 2:
-                new Pose3d();
-                break;
-            case 3:
-                new Pose3d();
-                break;
-            case 4:
-                new Pose3d();
-                break;
-            case 5:
-                new Pose3d();
-                break;
-            case 6:
-                new Pose3d();
-                break;
-        }
-    }
+
+
 }
