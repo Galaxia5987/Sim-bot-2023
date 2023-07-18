c513c33a91660464a32ed41d07d4f014beea425b
Alon Schwierz <@1>
2023-07-18 18:49:53 +0300

    add getInstance method to Vision class

--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -5,11 +5,33 @@ import edu.wpi.first.math.geometry.Quaternion;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import org.photonvision.PhotonCamera;
 
 public class Vision extends SubsystemBase {
     private final VisionIO.VisionInputs[] visionInputs;
     private final VisionIO[] io_s;
     private final Pose3d[] estimatedPoses;
+    private static Vision INSTANCE;
+
+    public static Vision getINSTANCE(){
+        if(INSTANCE == null) {
+            if (Robot.isReal()) {
+                INSTANCE = new Vision(
+                        new IOPhotonVision(new PhotonCamera("camera_1")),
+                        new IOPhotonVision(new PhotonCamera("camera_2"))
+                );
+
+            }
+            else{
+                INSTANCE = new Vision(
+                        new IOPhotonVision(new PhotonCamera("SimCamera_1")),
+                        new IOPhotonVision(new PhotonCamera("SimCamera_2"))
+                );
+            }
+        }
+        return INSTANCE;
+    }
 
     private Vision(VisionIO... io_s) {
         this.visionInputs = new VisionIO.VisionInputs[io_s.length];
