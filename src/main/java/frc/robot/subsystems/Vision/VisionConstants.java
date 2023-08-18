4e5a1006fc58d553036766db2fa06d0550a2fa45
GaiaZano05 <gaya.zano11@gmail.com>
2023-08-18 12:02:27 +0300

    Changing vision constants

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -9,7 +9,7 @@ public class VisionConstants {
     public static final double CAMERA_HEIGHT = 0.92;
     public static final double TARGET_HEIGHT_FROM_GROUND = 1.2;
     public static final double CAMERA_PITCH = 0;
-    public static final Transform3d ROBOT_TO_CAM = new Transform3d(new Pose3d(1,1,1, new Rotation3d(4,0,0)),new Pose3d(0,0,0, new Rotation3d(3,4,6)));
+    public static final Transform3d ROBOT_TO_CAM = new Transform3d(new Pose3d(1,0.3,0.3, new Rotation3d(0,0,0)),new Pose3d(0,0,0, new Rotation3d(0,0,0)));
     public static Pose3d TARGET_POSITION = new Pose3d(1, 1.2, 1, new Rotation3d(0,0,0));
 
 
