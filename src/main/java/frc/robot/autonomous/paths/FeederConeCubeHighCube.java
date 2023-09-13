18d4c960c7ebdf15e15e6419548a560b7017951d
GaiaZano05 <gaya.zano11@gmail.com>
2023-09-13 16:14:45 +0300

    Add autonomous paths

---
+++ src/main/java/frc/robot/autonomous/paths/FeederConeCubeHighCube.java
@@ -0,0 +1,61 @@
+package frc.robot.autonomous.paths;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autonomous.AutonUpperScoring;
+import frc.robot.autonomous.FollowPath;
+import frc.robot.autonomous.ResetAuto;
+import frc.robot.commandgroups.PickUpCube;
+import frc.robot.commandgroups.ReturnArm;
+import frc.robot.commandgroups.ReturnIntake;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.intake.commands.Retract;
+import frc.robot.subsystems.leds.command.PurpleLed;
+
+import static frc.robot.subsystems.intake.commands.Retract.Mode.DOWN;
+
+/**
+ * This class contains all parts of the path FeederConeCubeHigh.
+ * <p>
+ * In this path the robot places a cone in the grid that is closest to the feeder,
+ * goes to take a cube (the one closest to the feeder) and returns to place it.
+ */
+public class FeederConeCubeHighCube extends SequentialCommandGroup {
+    public FeederConeCubeHighCube() {
+        SwerveDrive swerveDrive = SwerveDrive.getInstance();
+        Gripper gripper = Gripper.getInstance();
+
+        addCommands(
+                new ResetAuto(),
+
+                new Retract(DOWN).withTimeout(0.35)
+                        .andThen(new AutonUpperScoring(true)),
+
+                new InstantCommand(gripper::open, gripper),
+
+                new ReturnArm().withTimeout(0.65),
+
+                new PurpleLed(),
+
+                FollowPath.loadTrajectory("FeederConeCubeHigh 1",
+                        FollowPath.resetCommand(swerveDrive)).alongWith(
+                        new PickUpCube().withTimeout(3.3)
+                ),
+
+                FollowPath.loadTrajectory("FeederConeCubeHigh 2")
+                        .alongWith(
+                                new ReturnIntake()
+                                        .andThen(new InstantCommand(gripper::close, gripper))
+                                        .andThen(new ReturnArm().withTimeout(0.65))
+                        ),
+
+                new AutonUpperScoring(false),
+                new InstantCommand(gripper::open, gripper),
+
+                new ReturnArm().withTimeout(0.65),
+
+                FollowPath.loadTrajectory("FeederConeCubeHigh 3").alongWith(new PickUpCube().withTimeout(5))
+        );
+    }
+}
---
+++ src/main/java/frc/robot/autonomous/paths/MiddleConeHighCubeEngage.java
@@ -0,0 +1,65 @@
+package frc.robot.autonomous.paths;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.autonomous.AutonUpperScoring;
+import frc.robot.autonomous.Engage;
+import frc.robot.autonomous.FollowPath;
+import frc.robot.autonomous.ResetAuto;
+import frc.robot.commandgroups.PickUpCube;
+import frc.robot.commandgroups.ReturnArm;
+import frc.robot.commandgroups.ReturnIntake;
+import frc.robot.subsystems.drivetrain.SwerveConstants;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.drivetrain.command.DriveTillPitch;
+import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.intake.commands.Retract;
+import frc.robot.subsystems.leds.command.PurpleLed;
+import frc.robot.utils.controllers.DieterController;
+
+import static frc.robot.subsystems.intake.commands.Retract.Mode.DOWN;
+
+public class MiddleConeHighCubeEngage extends SequentialCommandGroup {
+    private final DieterController yawController = new DieterController(3, 0, 0, 0);
+
+    public MiddleConeHighCubeEngage() {
+        SwerveDrive swerveDrive = SwerveDrive.getInstance();
+        Gripper gripper = Gripper.getInstance();
+        PathPlannerTrajectory trajectory = PathPlanner.loadPath("MiddleConeHighCubeEngage 1", new PathConstraints(SwerveConstants.MAX_VELOCITY_AUTO, SwerveConstants.MAX_ACCELERATION_AUTO));
+
+        addCommands(
+                new ResetAuto(),
+
+                FollowPath.resetCommand(swerveDrive).apply(trajectory),
+
+                new InstantCommand(gripper::close),
+
+                new Retract(DOWN).withTimeout(0.35)
+                        .andThen(new AutonUpperScoring(true)),
+
+                new InstantCommand(gripper::open),
+
+                new DriveTillPitch(8.5, 1.5)
+                        .alongWith(new ReturnArm().withTimeout(1)),
+
+                new PurpleLed(),
+
+                new RunCommand(() -> swerveDrive.drive(
+                                1.5,
+                                0,
+                                yawController.calculate(swerveDrive.getYaw(), 0),
+                                true
+                ), swerveDrive)
+                        .alongWith(new PickUpCube())
+                        .withTimeout(1.9),
+
+                new Engage(false, false)
+                        .alongWith(new ReturnIntake()
+                                .andThen(new InstantCommand(gripper::close)))
+        );
+    }
+}
