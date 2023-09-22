461c9327982445246db80b50f23faeff790f20b4
GaiaZano05 <gaya.zano11@gmail.com>
2023-09-22 13:09:46 +0300

    Add safety auto

---
+++ src/main/java/frc/robot/autonomous/SafetyAuto.java
@@ -0,0 +1,38 @@
+package frc.robot.autonomous;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commandgroups.GetArmIntoRobot;
+import frc.robot.commandgroups.ReturnArm;
+import frc.robot.subsystems.drivetrain.SwerveDrive;
+import frc.robot.subsystems.drivetrain.commands.DriveTillPitch;
+import frc.robot.subsystems.gripper.Gripper;
+import frc.robot.subsystems.intake.commands.Retract;
+
+import static frc.robot.subsystems.intake.commands.Retract.Mode.DOWN;
+
+public class SafetyAuto extends SequentialCommandGroup {
+
+    public SafetyAuto() { //TODO: check if it works
+        Gripper gripper = Gripper.getInstance();
+        SwerveDrive swerveDrive = SwerveDrive.getInstance();
+
+        addCommands(
+                new ResetAuto(),
+
+                new Retract(DOWN).withTimeout(0.35),
+
+                new AutonUpperScoring(true),
+
+                new InstantCommand(gripper::open),
+
+                new ReturnArm().withTimeout(1),
+
+                new GetArmIntoRobot().withTimeout(1.5),
+
+                new InstantCommand(swerveDrive::resetGyro),
+
+                new DriveTillPitch(10, 2).withTimeout(2.8)
+        );
+    }
+}
