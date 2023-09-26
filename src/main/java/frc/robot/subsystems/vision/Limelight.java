bf4885e4ee0faea61ba1cbc5c5fbc846587aed74
eitan <eitan.vichik@gmail.com>
2023-09-26 13:20:00 +0300

    Add Vision module

---
+++ src/main/java/frc/robot/subsystems/vision/Limelight.java
@@ -0,0 +1,300 @@
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.net.PortForwarder;
+import edu.wpi.first.networktables.*;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.subsystems.LoggedSubsystem;
+import frc.robot.utils.AllianceFlipUtil;
+
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+public class Limelight extends LoggedSubsystem<LimelightLogInputs> {
+    private static Limelight INSTANCE = null;
+
+    private final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    private final DoubleSubscriber tx = table.getDoubleTopic("tx").subscribe(0.0);
+    private final DoubleSubscriber ty = table.getDoubleTopic("ty").subscribe(0.0);
+    private final IntegerSubscriber tv = table.getIntegerTopic("tv").subscribe(0);
+    private final DoubleSubscriber ts = table.getDoubleTopic("ts").subscribe(0.0); // TODO: Check this value
+    private final IntegerSubscriber tid = table.getIntegerTopic("tid").subscribe(0);
+    private final IntegerSubscriber getpipe = table.getIntegerTopic("getpipe").subscribe(0); //TODO: check vision pipelines
+    private final DoubleArraySubscriber botPose = table.getDoubleArrayTopic("botpose_targetspace").subscribe(new double[6]);
+    private final DoubleArraySubscriber botPoseFieldOriented = table.getDoubleArrayTopic("botpose").subscribe(new double[6]);
+
+    private final AprilTagFieldLayout aprilTagFieldLayout;
+
+    private Limelight() {
+        super(new LimelightLogInputs());
+        for (int i = 5800; i <= 5805; i++) {
+            PortForwarder.add(i, "limelight.local", i);
+        }
+        try {
+            aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(
+                    AprilTagFields.k2023ChargedUp.m_resourceFile
+            );
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
+    }
+
+    /**
+     * If there is no instance of the limelight class it creates one and returns it
+     *
+     * @return Limelight instance
+     */
+    public static Limelight getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Limelight();
+        }
+        return INSTANCE;
+    }
+
+    public Pipeline getPipeline() {
+        return Pipeline.fromIndex((int) getpipe.get());
+    }
+
+    public void togglePipeline() {
+        var pipeline = getPipeline();
+        if (pipeline == Pipeline.APRIL_TAG_PIPELINE) {
+            setTapeMiddlePipeline();
+        } else {
+            setAprilTagsPipeline();
+        }
+    }
+
+    public void setAprilTagsPipeline() {
+        table.getEntry("pipeline").setNumber(Pipeline.APRIL_TAG_PIPELINE.index);
+    }
+
+    public void setTapeMiddlePipeline() {
+        table.getEntry("pipeline").setNumber(Pipeline.REFLECTIVE_TAPE_MIDDLE_PIPELINE.index);
+    }
+
+    public void setTapeTopPipeline() {
+        table.getEntry("pipeline").setNumber(Pipeline.REFLECTIVE_TAPE_TOP_PIPELINE.index);
+    }
+
+    /**
+     * Checks whether the limelight can see any targets
+     *
+     * @return whether the limelight detects any targets
+     */
+    public boolean hasTargets() {
+        return tv.get() > 0;
+    }
+
+    /**
+     * @return aprilTag id
+     */
+    public long getTagId() {
+        return tid.get();
+    }
+
+    /**
+     * @return pitch from camera to target
+     */
+    public Optional<Rotation2d> getPitch() {
+        if (!hasTargets()) {
+            return Optional.empty();
+        }
+        return Optional.of(Rotation2d.fromDegrees(tx.get()));
+    }
+
+    /**
+     * @return distance from target
+     */
+    public OptionalDouble getTargetDistance(double targetHeight) {
+        var pitch = getPitch();
+        if (!hasTargets() || pitch.isEmpty()) {
+            return OptionalDouble.empty();
+        }
+        double totalPitch = VisionConstants.CAMERA_PITCH + pitch.get().getRadians();
+        double error = VisionConstants.CAMERA_HEIGHT - targetHeight;
+        return OptionalDouble.of(Math.abs(error / Math.tan(totalPitch)));
+    }
+
+    public OptionalDouble getTargetDistance() {
+        if (getPipeline() == Pipeline.REFLECTIVE_TAPE_MIDDLE_PIPELINE) {
+            return getTargetDistance(58.5);
+        } else if (getPipeline() == Pipeline.REFLECTIVE_TAPE_TOP_PIPELINE) {
+            return getTargetDistance(110);
+        }
+        return OptionalDouble.empty();
+    }
+
+    /**
+     * @return robot yaw
+     */
+    public Optional<Rotation2d> getYaw() {
+        if (hasTargets()) {
+            double yaw = 0;
+            if (getPipeline() == Pipeline.APRIL_TAG_PIPELINE) {
+                yaw = VisionConstants.CAMERA_YAW.getDegrees() - ty.get();
+            } else {
+                yaw = ty.get();
+            }
+            return Optional.of(Rotation2d.fromDegrees(yaw));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Rotation2d> getAbsoluteYaw(Rotation2d gyroAngle) {
+        var yaw = getYaw();
+
+        return yaw.map(rotation2d -> VisionConstants.CAMERA_YAW.plus(gyroAngle).plus(rotation2d));
+    }
+
+    /**
+     * Estimates the position of the robot
+     *
+     * @param robotAngle The angle of the robot
+     * @return Optional Pose2d of the robot coordinates
+     */
+    public Optional<Pose2d> estimatePose(Rotation2d robotAngle, Pose3d target) {
+        if (!hasTargets()) {
+            return Optional.empty();
+        }
+        double absoluteAngle = robotAngle.getRadians() + getYaw().orElse(Rotation2d.fromDegrees(0)).getRadians();
+        double xTargetDistance = getTargetDistance(target.getZ()).orElse(0) * Math.sin(absoluteAngle);
+        double yTargetDistance = getTargetDistance(target.getZ()).orElse(0) * Math.cos(absoluteAngle);
+        double xDistance = xTargetDistance - target.getX();
+        double yDistance = yTargetDistance - target.getY();
+        Translation2d translation2d = new Translation2d(xDistance, yDistance);
+        return Optional.of(new Pose2d(translation2d, robotAngle));
+    }
+
+    public Optional<Pose2d> getAprilTagTarget(boolean rightSide, boolean useHorizontalOffset) {
+        int id = (int) getTagId();
+        var pose3d = aprilTagFieldLayout.getTagPose(id);
+        if (pose3d.isPresent()) {
+            var pose2d = pose3d.get().toPose2d();
+            Translation2d withOffset;
+
+            if (id < 5) {
+                if (id == 4) {
+                    withOffset = pose2d.getTranslation().minus(VisionConstants.DOUBLE_SUBSTATION_ADJUST_OFFSET);
+                    if (rightSide && useHorizontalOffset) {
+                        withOffset = withOffset.plus(VisionConstants.DOUBLE_SUBSTATION_HORIZONTAL_ADJUST_OFFSET);
+                    } else if (useHorizontalOffset) {
+                        withOffset = withOffset.minus(VisionConstants.DOUBLE_SUBSTATION_HORIZONTAL_ADJUST_OFFSET);
+                    }
+                } else {
+                    withOffset = pose2d.getTranslation().minus(VisionConstants.TARGET_ADJUST_OFFSET);
+                }
+            } else {
+                if (id == 8) {
+                    withOffset = pose2d.getTranslation().plus(VisionConstants.DOUBLE_SUBSTATION_ADJUST_OFFSET);
+                    if (rightSide && useHorizontalOffset) {
+                        withOffset = withOffset.plus(VisionConstants.DOUBLE_SUBSTATION_HORIZONTAL_ADJUST_OFFSET);
+                    } else if (useHorizontalOffset) {
+                        withOffset = withOffset.minus(VisionConstants.DOUBLE_SUBSTATION_HORIZONTAL_ADJUST_OFFSET);
+                    }
+                } else {
+                    withOffset = pose2d.getTranslation().plus(VisionConstants.TARGET_ADJUST_OFFSET);
+                }
+            }
+
+            return Optional.of(new Pose2d(withOffset, pose2d.getRotation().plus(Rotation2d.fromDegrees(180))));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Pose2d> getAprilTagTarget() {
+        return getAprilTagTarget(true, false);
+    }
+
+    public Optional<Pose2d> getAprilTagTarget(DriverStation.Alliance alliance) {
+        var target = getAprilTagTarget();
+        if (target.isPresent()) {
+            var pose = AllianceFlipUtil.apply(alliance, target.get());
+            return Optional.of(pose);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Pose2d> getBotPose() {
+        long id = getTagId();
+        if (id > 0 && id < 9) {
+            return Optional.of(
+                    new Pose2d(botPose.get()[2], -botPose.get()[0], Rotation2d.fromDegrees(botPose.get()[4]))
+            );
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Pose2d> getBotPoseFieldOriented() {
+        var id = getTagId();
+        if (id > 0 && id < 9) {
+            var botPoseResult = DriverStation.getAlliance() == DriverStation.Alliance.Blue ?
+                    LimelightHelpers.getBotPose2d_wpiBlue("") :
+                    LimelightHelpers.getBotPose2d_wpiRed("");
+            return Optional.of(botPoseResult);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Pose2d> getBotPose(DriverStation.Alliance alliance) {
+        var botPose = getBotPose();
+        if (botPose.isPresent()) {
+            var pose = AllianceFlipUtil.apply(alliance, botPose.get());
+            return Optional.of(pose);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Pose2d> positionForId(int id) {
+        var pose = aprilTagFieldLayout.getTagPose(id);
+        aprilTagFieldLayout.setOrigin(
+                AprilTagFieldLayout.OriginPosition.kBlueAllianceWallRightSide);
+        return pose.map(pose3d -> AllianceFlipUtil.apply(DriverStation.getAlliance(), pose3d.toPose2d()));
+    }
+
+    @Override
+    public String getSubsystemName() {
+        return "Limelight";
+    }
+
+    /**
+     * Updates the limelight log inputs.
+     */
+    @Override
+    public void updateInputs() {
+        loggerInputs.hasTargets = hasTargets();
+        getYaw().ifPresent((value) -> loggerInputs.yaw = value.getDegrees());
+        loggerInputs.tagId = getTagId();
+        getTargetDistance(VisionConstants.UPPER_CONE_TARGET_TAPE_HEIGHT).ifPresent((value) -> loggerInputs.highTargetDistance = value);
+        getTargetDistance(VisionConstants.LOWER_CONE_TARGET_TAPE_HEIGHT).ifPresent((value) -> loggerInputs.lowTargetDistance = value);
+        getAprilTagTarget().ifPresent((value) -> loggerInputs.aprilTagTarget = value);
+    }
+
+    public enum Pipeline {
+        APRIL_TAG_PIPELINE(0),
+        REFLECTIVE_TAPE_MIDDLE_PIPELINE(2),
+        REFLECTIVE_TAPE_TOP_PIPELINE(3);
+
+        public final int index;
+
+        Pipeline(int index) {
+            this.index = index;
+        }
+
+        public static Pipeline fromIndex(int index) {
+            switch (index) {
+                case 2:
+                    return REFLECTIVE_TAPE_MIDDLE_PIPELINE;
+                case 3:
+                    return REFLECTIVE_TAPE_TOP_PIPELINE;
+                default:
+                    return APRIL_TAG_PIPELINE;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -0,0 +1,56 @@
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.math.geometry.*;
+
+public class VisionConstants {
+    public static final double CAMERA_HEIGHT = 0.76; //[m]
+    public static final double CAMERA_PITCH = 0.0; //[radian]
+    public static final double UPPER_CONE_TARGET_TAPE_HEIGHT = 1.17; //[m]
+    public static final double LOWER_CONE_TARGET_TAPE_HEIGHT = 0.65; //[m]
+    public static final double FIELD_LENGTH = 16.54;
+    public static final double FIELD_WIDTH = 8.02;
+    public static final Pose2d CENTER_POSE = new Pose2d(FIELD_LENGTH / 2, FIELD_WIDTH / 2, Rotation2d.fromDegrees(0));
+    public static final Translation2d DOUBLE_SUBSTATION_ADJUST_OFFSET = new Translation2d(1.0, 0);
+    public static final Translation2d TARGET_ADJUST_OFFSET = new Translation2d(1.0, 0);
+    public static final Translation2d CUBE_ID1_POSE = CENTER_POSE.getTranslation().plus(new Translation2d(7.24310, -2.93659)).minus(TARGET_ADJUST_OFFSET);
+    public static final Translation2d CUBE_ID2_POSE = CENTER_POSE.getTranslation().plus(new Translation2d(7.24310, -1.26019)).minus(TARGET_ADJUST_OFFSET);
+    public static final Translation2d CUBE_ID3_POSE = CENTER_POSE.getTranslation().plus(new Translation2d(7.24310, 0.41621)).minus(TARGET_ADJUST_OFFSET);
+    //    public static final Translation2d DOUBLE_SUBSTATION_ID4_POSE = CENTER_POSE.getTranslation().plus(new Translation2d(7.90832, 2.74161)).minus(DOUBLE_SUBSTATION_ADJUST_OFFSET);
+    public static final Translation2d DOUBLE_SUBSTATION_ID4_POSE = new Translation2d(6.77, 4.55);
+    // Red april tag targets
+    //    public static final Translation2d CUBE_ID1_POSE = CENTER_POSE.plus(new Translation2d(5.8, -2.93659));
+    public static final Translation2d DOUBLE_SUBSTATION_HORIZONTAL_ADJUST_OFFSET = new Translation2d(1.0, 0);
+    public static final Translation2d CENTER_TRANSLATION = new Translation2d(FIELD_LENGTH / 2, FIELD_WIDTH / 2);
+    // Blue april tag targets
+    public static final Translation2d CUBE_ID5_POSE = CENTER_TRANSLATION.plus(new Translation2d(-7.24310, 2.93659)).plus(TARGET_ADJUST_OFFSET);
+    public static final Translation2d CUBE_ID6_POSE = CENTER_TRANSLATION.plus(new Translation2d(-7.24310, 1.26019)).plus(TARGET_ADJUST_OFFSET);
+    public static final Translation2d CUBE_ID7_POSE = CENTER_TRANSLATION.plus(new Translation2d(-7.24310, -0.41621)).plus(TARGET_ADJUST_OFFSET);
+    public static final Translation2d DOUBLE_SUBSTATION_ID8_POSE = CENTER_TRANSLATION.plus(new Translation2d(-7.90832, -2.74161)).plus(DOUBLE_SUBSTATION_ADJUST_OFFSET);
+    // Red april tag targets
+
+    public static final Pose3d UPPER_CONE_LEFT_GRID_TARGET = new Pose3d(16.178, 4.99, 1.17, new Rotation3d());
+    public static final Rotation2d CAMERA_YAW = Rotation2d.fromDegrees(16.62);
+
+    public static Translation2d getTargetDesiredTranslation(int id) {
+        switch (id) {
+            case 1:
+                return CUBE_ID1_POSE;
+            case 2:
+                return CUBE_ID2_POSE;
+            case 3:
+                return CUBE_ID3_POSE;
+            case 4:
+                return DOUBLE_SUBSTATION_ID4_POSE;
+            case 5:
+                return CUBE_ID5_POSE;
+            case 6:
+                return CUBE_ID6_POSE;
+            case 7:
+                return CUBE_ID7_POSE;
+            case 8:
+                return DOUBLE_SUBSTATION_ID8_POSE;
+            default:
+                return null;
+        }
+    }
+}
