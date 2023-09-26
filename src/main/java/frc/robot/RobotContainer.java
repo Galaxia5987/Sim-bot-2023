17a0c686123ad3d73038a760b48cdece488d438a
GaiaZano05 <gaya.zano11@gmail.com>
2023-09-26 13:14:49 +0300

    Change auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -121,6 +121,6 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return new FeederConeCubeHighCube();
+        return new BumperConeCubeHighCube();
     }
 }
