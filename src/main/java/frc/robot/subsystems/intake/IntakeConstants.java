cc70fc4fc9180e40292f2fea45cb6612fe622f16
GaiaZano05 <gaya.zano11@gmail.com>
2023-10-01 12:02:22 +0300

    Fix intake constants

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -4,15 +4,15 @@ import frc.robot.utils.units.Units;
 
 public class IntakeConstants {
     public static final double GEAR_RATIO = 35.26;
-    public static final double kP = 0.15;
+    public static final double kP = 0.08;
     public static final double kI = 0;
     public static final double kD = 0;
-    public static final double kF = 0;
+    public static final double kF = 0.05;
 
     public static final double FALCON_TICKS_PER_ROTATION = 2048;
     public static final double TICKS_PER_DEGREE = FALCON_TICKS_PER_ROTATION / 360 * GEAR_RATIO;
 
-    public static final double ANGLE_UP = -5.5; //[deg]
+    public static final double ANGLE_UP = 8; //[deg]
     public static final double ANGLE_DOWN = -99; //[deg]
 
     public static final double INTAKE_POWER = 0.8; //[%]
@@ -20,5 +20,5 @@ public class IntakeConstants {
     public static final double INTAKE_ANGLE_MAX_ACCELERATION = INTAKE_ANGLE_VELOCITY / 0.5; //[RPM/s]
     public static final double MAX_CURRENT = 10;
 
-    public static final double ANGLE_MOTOR_POWER = 0.6;
+    public static final double ANGLE_MOTOR_POWER = 0.3;
 }
