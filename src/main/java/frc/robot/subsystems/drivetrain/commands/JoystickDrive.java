582bf0b2fab664a2ce79cd17c51b0903c9ee701f
GaiaZano05 <gaya.zano11@gmail.com>
2023-10-03 17:44:52 +0300

    Add option to hold the swerve in a heading of 180 degrees

--- src/main/java/frc/robot/subsystems/drivetrain/commands/JoystickDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/commands/JoystickDrive.java
@@ -1,14 +1,22 @@
 package frc.robot.subsystems.drivetrain.commands;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Ports;
+import frc.robot.subsystems.drivetrain.SwerveConstants;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 
 public class JoystickDrive extends CommandBase {
     private final SwerveDrive swerveDrive;
     private final Joystick joystick1;
     private final Joystick joystick2;
+
+    private boolean shouldHoldAngle = false;
+    private final PIDController omegaController = new PIDController(
+            SwerveConstants.OMEGA_kP, SwerveConstants.OMEGA_kI, SwerveConstants.OMEGA_kD);
 
     public JoystickDrive(SwerveDrive swerveDrive, Joystick joystick1, Joystick joystick2) {
         this.swerveDrive = swerveDrive;
@@ -19,11 +27,22 @@ public class JoystickDrive extends CommandBase {
 
     @Override
     public void execute() {
-        swerveDrive.drive(
-                MathUtil.applyDeadband(-joystick1.getY(), 0.1),
-                MathUtil.applyDeadband(-joystick1.getX(), 0.1),
-                MathUtil.applyDeadband(-joystick2.getX(), 0.1),
-                true
-        );
+        shouldHoldAngle = new JoystickButton(joystick2, Ports.UI.JOYSTICK_RIGHT_BIG_BUTTON).getAsBoolean();
+        if (!shouldHoldAngle) {
+            swerveDrive.drive(
+                    MathUtil.applyDeadband(-joystick1.getY(), 0.1),
+                    MathUtil.applyDeadband(-joystick1.getX(), 0.1),
+                    MathUtil.applyDeadband(-joystick2.getX(), 0.1),
+                    true
+            );
+        }
+        else {
+            swerveDrive.drive(
+                    MathUtil.applyDeadband(-joystick1.getY(), 0.1),
+                    MathUtil.applyDeadband(-joystick1.getX(), 0.1),
+                    omegaController.calculate(swerveDrive.getYaw(), Math.toRadians(180)),
+                    true
+            );
+        }
     }
 }
