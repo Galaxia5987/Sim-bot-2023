9b5acab09000b0427d1074a1f6bed6f8af494588
Emma <emmalivne.03@gmail.com>
2023-10-20 13:30:54 +0300

    Update swerve offsets

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveConstants.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveConstants.java
@@ -7,7 +7,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 
 public class SwerveConstants {
     public static final double[] OFFSETS =
-            {0.56509977142749428, 0.26230635655765894, 0.930, 0.5489868137246704};
+            {0.5650298641257466,0.2622759815568995,0.008052200201305005,0.5445371386134285};
 
     public static final double VOLT_COMP_SATURATION = 12;
     public static final SupplyCurrentLimitConfiguration SUPPLY_CURRENT_LIMIT = new SupplyCurrentLimitConfiguration(true, 50, 0, 0);
