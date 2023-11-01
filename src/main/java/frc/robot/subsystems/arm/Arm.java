70db7eca874bb53bdcf48cb691491fd10ab58df0
Alon Schwierz <@1>
2023-11-01 09:31:58 +0200

    /

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -3,36 +3,45 @@ package frc.robot.subsystems.arm;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
+    static Arm INSTANCE;
     private ArmIO io;
     private ArmInputsAutoLogged inputs;
-    static Arm INSTANCE;
 
-public Arm getINSTANCE(){
-    if(INSTANCE == null){
-        INSTANCE = new Arm(io);
+    private Arm(ArmIO io) {
+        this.io = io;
     }
+
+    public static Arm getINSTANCE() {
+        if (INSTANCE == null) {
+            INSTANCE = new Arm(new ArmIO());
+        }
 
         return INSTANCE;
 
-}
+    }
 
-private void setElbowPower(double power){
-    inputs.
-}
+    private void setElbowPower(double power) {
+        inputs.elbowAppliedVoltage = power * 12;
+    }
 
+    private void setElbowAngleRelative(double angle) {
+        inputs.elbowAngleRelative = angle;
+    }
 
+    private void setShoulderPower(double power) {
+        inputs.elbowAppliedVoltage = power * 12;
+    }
 
-
-    private Arm(ArmIO io){
-        this.io = io;
+    private void setShoulderAngle(double angle) {
+        inputs.elbowAngleRelative = angle;
     }
 
     @Override
     public void periodic() {
-io.setElbowAngle();
-io.setElbowPower();
-io.setShoulderAngle();
-io.setShoulderPower();
-io.updateInputs();
+        io.setElbowAngle(inputs.elbowAngleRelative);
+        io.setElbowPower(inputs.elbowAppliedVoltage);
+        io.setShoulderAngle(inputs.shoulderAngle);
+        io.setShoulderPower(inputs.shoulderAppliedVoltage);
+        io.updateInputs();
     }
 }
---
+++ src/main/java/frc/robot/subsystems/arm/commands/setTipPosition.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.arm.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.arm.Arm;
+
+public class setTipPosition extends CommandBase {
+    private Arm arm = Arm.get
+}
