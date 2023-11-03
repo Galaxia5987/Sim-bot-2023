56b06e53a16b847145245f3499e44a5cb36af6fd
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-11-03 13:05:57 +0200

    Updated to simulation structure

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,56 +1,30 @@
 package frc.robot.subsystems.intake;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.Ports;
+import frc.robot.Robot;
 import frc.robot.subsystems.intake.commands.HoldIntakeInPlace;
-import frc.robot.utils.units.UnitModel;
 import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
+    private final IntakeIO io;
     private static Intake INSTANCE;
-    private final CANSparkMax motor = new CANSparkMax(Ports.Intake.INTAKE_MOTOR, CANSparkMaxLowLevel.MotorType.kBrushless);
-    private final TalonFX angleMotor = new TalonFX(Ports.Intake.ANGLE_MOTOR);
-    private final UnitModel unitModel = new UnitModel(IntakeConstants.TICKS_PER_DEGREE);
     private ControlMode angleMode;
     private final IntakeLoggedInputs inputs = new IntakeLoggedInputs();
     private Command lastCommand = null;
     private boolean switchedToDefaultCommand = false;
 
-    private Intake() {
-        angleMotor.configFactoryDefault();
-        motor.restoreFactoryDefaults();
 
-        motor.setIdleMode(CANSparkMax.IdleMode.kCoast);
-        motor.enableVoltageCompensation(Constants.NOMINAL_VOLTAGE);
-        motor.setInverted(Ports.Intake.POWER_INVERTED);
-        for (int i = 1; i <= 6; i++) {
-            motor.setPeriodicFramePeriod(CANSparkMaxLowLevel.PeriodicFrame.fromId(i), 50000);
+    private Intake(){
+        if (Robot.isReal()){
+            io = new IntakeIOReal();
         }
-        motor.burnFlash();
-
-        angleMotor.setNeutralMode(NeutralMode.Brake);
-        angleMotor.enableVoltageCompensation(true);
-        angleMotor.configVoltageCompSaturation(Constants.NOMINAL_VOLTAGE);
-        angleMotor.setInverted(Ports.Intake.ANGLE_INVERTED);
-        angleMotor.config_kP(0, IntakeConstants.kP);
-        angleMotor.config_kI(0, IntakeConstants.kI);
-        angleMotor.config_kD(0, IntakeConstants.kD);
-        angleMotor.config_kF(0, IntakeConstants.kF);
-        angleMotor.configStatorCurrentLimit(new StatorCurrentLimitConfiguration(
-                true, 30, 0, 0
-        ));
-        angleMotor.configClosedLoopPeakOutput(0, 0.4);
+        else{
+            io = new IntakeIOSim();
+        }
     }
 
     /**
@@ -67,8 +41,18 @@ public class Intake extends SubsystemBase {
      * @return the relative output.
      * Return the power that the motor applies. [%]
      */
-    private double getPower() {
-        return motor.get();
+    private double getSpinMotorPower() {
+        return inputs.spinMotorPower;
+    }
+    public double getAngleMotorAngle() {
+        return inputs.angleMotorAngle;
+    }
+
+    private double getAngleMotorVelocity() {
+        return inputs.angleMotorVelocity;
+    }
+    public double getCurrent() {
+        return inputs.angleMotorcurrent;
     }
 
     /**
@@ -76,8 +60,8 @@ public class Intake extends SubsystemBase {
      *
      * @param power is the power that the motor applies. [%]
      */
-    public void setPower(double power) {
-        inputs.setpointPower = power;
+    public void setSpinMotorPower(double power) {
+        inputs.setpointSpinMotorPower = power;
     }
 
     /**
@@ -85,60 +69,37 @@ public class Intake extends SubsystemBase {
      *
      * @param angle is the angle of the retractor. [degrees]
      */
-    public void setAngle(double angle) {
-        inputs.setpointAngle = angle;
+    public void setAngleMotorAngle(double angle) {
+        inputs.setpointAngleMotorAngle = angle;
         angleMode = ControlMode.Position;
     }
 
     public void setAnglePower(double power) {
-        inputs.setpointAnglePower = power;
+        inputs.setpointAngleMotorPower = power;
         angleMode = ControlMode.PercentOutput;
     }
-
-    /**
-     * @return the motor's position. [degrees]
-     */
-    public double getAngle() {
-        return inputs.angle;
-    }
-
-    private double getAngleMotorVelocity() {
-        return inputs.velocity;
+    public void resetEncoder(double angle) {
+        io.resetEncoder(angle);
     }
 
     public Command lowerIntake() {
         return new InstantCommand(() -> resetEncoder(IntakeConstants.ANGLE_UP), this)
-                .andThen(new RunCommand(() -> setAngle(-40), this));
+                .andThen(new RunCommand(() -> setAngleMotorAngle(-40), this));
     }
-
-    public void resetEncoder(double angle) {
-        angleMotor.setSelectedSensorPosition(
-                unitModel.toTicks(angle)
-        );
-    }
-
-    public double getCurrent() {
-        return inputs.current;
-    }
-
     public Command run(double power) {
-        return new RunCommand(() -> this.setPower(power));
+        return new RunCommand(() -> this.setSpinMotorPower(power));
     }
 
     @Override
     public void periodic() {
-        inputs.power = motor.getAppliedOutput();
-        inputs.angle = unitModel.toUnits(angleMotor.getSelectedSensorPosition());
-        inputs.velocity = unitModel.toVelocity(angleMotor.getSelectedSensorVelocity());
-        inputs.current = angleMotor.getSupplyCurrent();
-        inputs.anglePower = angleMotor.getMotorOutputPercent();
+        io.updateInputs(inputs);
+        io.setSpinMotorPower(inputs.setpointSpinMotorPower);
 
-        motor.set(inputs.setpointPower);
         if (angleMode == ControlMode.Position){
-            angleMotor.set(ControlMode.Position, unitModel.toTicks(inputs.setpointAngle));
+            io.setAngleMotorAngle(inputs.setpointAngleMotorAngle);
         }
         else {
-            angleMotor.set(TalonFXControlMode.PercentOutput, inputs.setpointAnglePower);
+            io.setAngleMotorPower(inputs.setpointSpinMotorPower);
         }
 
 
