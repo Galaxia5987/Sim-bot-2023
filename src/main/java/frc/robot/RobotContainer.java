68864308a6776f3066842249db2293ef5bb528b9
eitan <eitan.vichik@gmail.com>
2023-11-08 11:46:26 +0200

    Change to joystick drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,15 +1,18 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.common.swerve.SwerveDrive;
-import frc.robot.common.swerve.commands.XboxDrive;
+import frc.robot.common.swerve.commands.JoystickDrive;
 
 public class RobotContainer {
     private static RobotContainer INSTANCE = null;
 
     private final SwerveDrive drive = SwerveDrive.getInstance();
     private final XboxController xboxController = new XboxController(0);
+    private final Joystick leftJoystick = new Joystick(1);
+    private final Joystick rightJoystick = new Joystick(2);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -28,7 +31,7 @@ public class RobotContainer {
 
     private void configureDefaultCommands() {
         drive.setDefaultCommand(
-                new XboxDrive(drive, xboxController)
+                new JoystickDrive(drive, leftJoystick, rightJoystick)
         );
     }
 
