f88204dfaa22c29c70a9ea210ec455883b5101a6
GaiaZano05 <gaya.zano11@gmail.com>
2023-11-10 13:46:01 +0200

    Update camera locations

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -3,7 +3,14 @@ package frc.robot.subsystems.vision;
 import edu.wpi.first.math.geometry.*;
 
 public class VisionConstants {
-    public static final Transform3d[] ROBOT_TO_CAM = new Transform3d[]{new Transform3d(new Translation3d(0, 0, 0.45), new Rotation3d(0, 0, 180)), new Transform3d(new Translation3d(0, 0, 0.45), new Rotation3d(0, 0, 0))};
+    public static final Transform3d[] ROBOT_TO_CAM = new Transform3d[]{
+            new Transform3d( //left cam
+                    new Translation3d(-0.249, 0.30397, 0.5023),
+                    new Rotation3d(0, 0, 142.5)),
+            new Transform3d( //right cam
+                    new Translation3d(-0.2363, -0.527, 0.196),
+                    new Rotation3d(0, 28, -151))};
+
     public static final Transform3d LIME_OFFSET = new Transform3d(new Translation3d( (16.54) / 2,(8.02) / 2, 0), new Rotation3d());
     public static final double TARGET_WIDTH = 0.27;
     public static final double TARGET_LENGTH = 0.27;
