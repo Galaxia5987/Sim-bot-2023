60c1c8245b1f60a4d01bf07a0cf711d0edf0bc18
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-11-10 12:08:13 +0200

    add joystickTrigger

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,16 +3,22 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.common.swerve.SwerveDrive;
 import frc.robot.common.swerve.commands.JoystickDrive;
+import frc.robot.subsystems.vision.Vision;
 
 public class RobotContainer {
     private static RobotContainer INSTANCE = null;
 
     private final SwerveDrive drive = SwerveDrive.getInstance();
+    private final Vision vision = Vision.getInstance();
     private final XboxController xboxController = new XboxController(0);
     private final Joystick leftJoystick = new Joystick(1);
     private final Joystick rightJoystick = new Joystick(2);
+
+    private final JoystickButton joystickTrigger = new JoystickButton(leftJoystick, 1);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -36,6 +42,7 @@ public class RobotContainer {
     }
 
     private void configureButtonBindings() {
+        joystickTrigger.onTrue(new InstantCommand(drive::resetGyro));
     }
 
 
