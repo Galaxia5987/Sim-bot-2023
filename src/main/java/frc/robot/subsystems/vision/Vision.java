86ed0fc98797060e85223cda1c2d01016346ec8f
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-11-10 12:07:58 +0200

    fix vision log

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -40,7 +40,7 @@ public class Vision extends SubsystemBase {
         for (int i = 0; i < modules.length; i++) {
             VisionModule module = modules[i];
             module.io.updateInputs(module.inputs);
-            Logger.getInstance().processInputs("Vision/" + module.name, module.inputs);
+            Logger.getInstance().processInputs(module.name, module.inputs);
             results[i] = module.io.getLatestResult();
         }
     }
