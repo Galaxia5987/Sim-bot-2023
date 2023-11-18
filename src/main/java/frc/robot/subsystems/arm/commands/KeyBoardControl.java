892a9665dcfca26690952e1a3ae6af02418b04af
Alon Schwierz <@1>
2023-11-18 23:05:13 +0200

    /

--- src/main/java/frc/robot/subsystems/arm/commands/KeyBoardControl.java
+++ src/main/java/frc/robot/subsystems/arm/commands/KeyBoardControl.java
@@ -1,11 +1,8 @@
 package frc.robot.subsystems.arm.commands;
 
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.Arm;
-import frc.robot.subsystems.arm.ArmIO;
-import frc.robot.subsystems.arm.ArmKinematics;
 
 public class KeyBoardControl extends CommandBase {
     private final GenericHID controller = new GenericHID(0);
@@ -19,17 +16,16 @@ public class KeyBoardControl extends CommandBase {
     public void execute() {
         if (controller.getRawButton(1)) {
             arm.setElbowPower(0.001);
-            ;
-            if (controller.getRawButton(3)) {
-                arm.setElbowPower(-0.001);
+        }
+        if (controller.getRawButton(2)) {
+            arm.setElbowPower(-0.001);
 
-            }
-            if (controller.getRawButton(2)) {
-                arm.setShoulderPower(0.001);
-            }
-            if (controller.getRawButton(2)) {
-                arm.setShoulderPower(0.001);
-            }
+        }
+        if (controller.getRawButton(3)) {
+            arm.setShoulderPower(0.001);
+        }
+        if (controller.getRawButton(4)) {
+            arm.setShoulderPower(-0.001);
         }
     }
 }
