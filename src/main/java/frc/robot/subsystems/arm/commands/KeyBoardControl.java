d35a2964d10518f1ecc5f061c6cf4cb69bb3c214
rakrakon <68773850+rakrakon@users.noreply.github.com>
2023-11-21 17:09:30 +0200

    Update KeyBoardControl.java

--- src/main/java/frc/robot/subsystems/arm/commands/KeyBoardControl.java
+++ src/main/java/frc/robot/subsystems/arm/commands/KeyBoardControl.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.ArmConstants;
 import frc.robot.subsystems.arm.ArmIO;
 
 public class KeyBoardControl extends CommandBase {
@@ -17,10 +18,10 @@ public class KeyBoardControl extends CommandBase {
     @Override
     public void execute() {
         if (controller.getRawButton(1)) {
-            arm.setEndEffectorPosition(new Translation2d(1, 1), ArmIO.armKenematics);
+            arm.setEndEffectorPosition(ArmConstants.FEEDER_POSITION, ArmIO.armKenematics);
         }
         if (controller.getRawButton(2)) {
-            arm.setEndEffectorPosition(new Translation2d(-1, 1), ArmIO.armKenematics);
+            arm.setEndEffectorPosition(ArmConstants.STARTING_POSITION, ArmIO.armKenematics);
         }
         if (controller.getRawButton(3)) {
             arm.setShoulderPower(0.001);
