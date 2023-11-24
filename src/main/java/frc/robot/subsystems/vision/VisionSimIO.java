7c37037d993b61aa0cdc80d544640b29422a1a07
Alon Schwierz <@1>
2023-11-24 11:47:32 +0200

    .

--- src/main/java/frc/robot/subsystems/vision/VisionSimIO.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSimIO.java
@@ -1,18 +1,20 @@
 package frc.robot.subsystems.vision;
 
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import org.photonvision.SimPhotonCamera;
 import org.photonvision.SimVisionSystem;
 
-public class VisionSimIO implements VisionIO{
+public class VisionSimIO implements VisionIO {
     private SimVisionSystem simVisionSystem;
+    private SimPhotonCamera simCamera;
+    private NetworkTableEntry tableEntry = new NetworkTableEntry()
 
-
-    private VisionSimIO(Transform3d robotToCam){
+    public VisionSimIO(Transform3d robotToCam) {
         simVisionSystem = new SimVisionSystem("simCam", 95, robotToCam, 1000, 1600, 1200, 0);
+        simCamera = new SimPhotonCamera(NetworkTableInstance.getDefault(), "simCam");
     }
-
-
-
 
 
     @Override
