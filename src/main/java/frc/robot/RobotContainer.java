d8994f3ce0fc33fb4af3a4636e6405f4a416aff1
eitan <eitan.vichik@gmail.com>
2023-11-27 16:20:40 +0200

    Use common as library

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,18 +3,15 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.common.swerve.SwerveDrive;
-import frc.robot.common.swerve.commands.JoystickDrive;
-import frc.robot.common.swerve.commands.KeyboardDriveSim;
-import frc.robot.common.swerve.commands.XboxDrive;
 import frc.robot.subsystems.vision.Vision;
+import swerve.SwerveDrive;
+import swerve.commands.XboxDrive;
 
 public class RobotContainer {
     private static RobotContainer INSTANCE = null;
 
-    private final SwerveDrive drive = SwerveDrive.getInstance();
+    private final SwerveDrive drive = SwerveDrive.getInstance(Robot.isReal());
     private final Vision vision = Vision.getInstance();
     private final XboxController xboxController = new XboxController(0);
     private final Joystick leftJoystick = new Joystick(1);
