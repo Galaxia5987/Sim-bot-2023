3919d2ba3eae4912936337e3968a0dad576832fb
Alon Schwierz <@1>
2023-12-03 19:54:12 +0200

    I love pop music

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,30 +1,49 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.system.plant.LinearSystemId;
-import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 
 public class IntakeIOSim implements IntakeIO {
-    private final FlywheelSim spinMotor;
-    private final SingleJointedArmSim angleMotor;
-    private final PIDController angleFeedback;
+    //    private final FlywheelSim spinMotor;
+//    private final SingleJointedArmSim angleMotor;
+    private final TalonFXSimState angleMotorPhx;
+    private final TalonFX talonFXAngle;
+    private final TalonFX talonFXPower;
+    private final TalonFXSimState powerMotorPhx;
+    private final TalonFXConfigurator angleConfigurator;
+    private final TalonFXConfigurator powerConfigurator;
+//    private final PIDController angleFeedback;
 
     public IntakeIOSim() {
-        spinMotor = new FlywheelSim(DCMotor.getNEO(1), IntakeConstants.SPIN_GEAR_RATIO, 1);
-        angleMotor = new SingleJointedArmSim(LinearSystemId.createSingleJointedArmSystem(
-                DCMotor.getFalcon500(1), 2, IntakeConstants.ANGLE_GEAR_RATIO
-        ), DCMotor.getFalcon500(1), IntakeConstants.ANGLE_GEAR_RATIO, 0.7, -Math.PI * 2, Math.PI * 2, false, 0); //could be wrong type of motor or max/min angle
-        angleFeedback = new PIDController(IntakeConstants.kP, IntakeConstants.kI, IntakeConstants.kD);
+//        spinMotor = new FlywheelSim(DCMotor.getNEO(1), IntakeConstants.SPIN_GEAR_RATIO, 1);
+//        angleMotor = new SingleJointedArmSim(LinearSystemId.createSingleJointedArmSystem(
+//                DCMotor.getFalcon500(1), 2, IntakeConstants.ANGLE_GEAR_RATIO
+//        ), DCMotor.getFalcon500(1), IntakeConstants.ANGLE_GEAR_RATIO, 0.7, -Math.PI * 2, Math.PI * 2, false, 0); //could be wrong type of motor or max/min angle
+//        angleFeedback = new PIDController(IntakeConstants.kP, IntakeConstants.kI, IntakeConstants.kD);
+
+        talonFXAngle = new TalonFX(0);
+        talonFXPower = new TalonFX(1);
+
+
+        angleMotorPhx = new TalonFXSimState(talonFXAngle);
+        powerMotorPhx = new TalonFXSimState(talonFXPower);
+
+        angleConfigurator = talonFXAngle.getConfigurator();
+        powerConfigurator = talonFXPower.getConfigurator();
+
+
     }
 
     @Override
     public void updateInputs(IntakeLoggedInputs inputs) {
-        inputs.spinMotorPower = inputs.setpointSpinMotorPower;
-        inputs.spinMotorCurrent = spinMotor.getCurrentDrawAmps();
-        inputs.angleMotorAngle = angleMotor.getAngleRads();
+        inputs.spinMotorPower = powerMotorPhx.getMotorVoltage()/12;
+        inputs.spinMotorCurrent = powerMotorPhx.getSupplyCurrent();
+        inputs.angleMotorAngle = angleMotorPhx.setRotorVelocity()
         inputs.angleMotorcurrent = angleMotor.getCurrentDrawAmps();
+
+
 
         spinMotor.update(0.02);
         angleMotor.update(0.02);
@@ -32,22 +51,17 @@ public class IntakeIOSim implements IntakeIO {
 
     @Override
     public void setSpinMotorPower(double power) {
-        spinMotor.setInputVoltage(power * 12);
+        powerMotorPhx.setSupplyVoltage(power*12);
     }
 
     @Override
     public void setAngleMotorAngle(double angle) {
-        double angleMotorAppliedVoltage = angleFeedback.calculate(angleMotor.getAngleRads(), angle);
-        angleMotor.setInputVoltage(angleMotorAppliedVoltage);
+        angleMotorPhx.setRawRotorPosition(angle/2*Math.PI);
     }
 
-    @Override
-    public void setAngleMotorPower(double power) {
-        angleMotor.setInputVoltage(power * 12);
-    }
 
     @Override
     public void resetEncoder(double angle) {
-
+return super;
     }
 }
