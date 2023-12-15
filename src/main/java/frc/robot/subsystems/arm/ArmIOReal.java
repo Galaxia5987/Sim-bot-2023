a0e58c8c44500f73f3120dd837dbca31284896b6
GaiaZano05 <gaya.zano11@gmail.com>
2023-12-15 13:23:01 +0200

    Changing functions to work with phoenix6

--- src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOReal.java
@@ -5,14 +5,12 @@ import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.Slot1Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.ControlRequest;
-import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.controls.StrictFollower;
-import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.controls.*;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -129,8 +127,15 @@ public class ArmIOReal implements ArmIO {
      * @param power desired power. [-1,1]
      */
     public void setShoulderJointPower(double power) {
-        motorControlRequest = new VoltageOut()
-        shoulderMainMotor.set(power);
+        shoulderMainMotor.setControl(new DutyCycleOut(power));
+    }
+
+    public double getShoulderJointPower() {
+        return shoulderMainMotor.get();
+    }
+
+    public double getElbowJointPower() {
+        return elbowMainMotor.get();
     }
 
     /**
@@ -139,17 +144,10 @@ public class ArmIOReal implements ArmIO {
      * @param power desired power. [-1,1]
      */
     public void setElbowJointPower(double power) {
-        elbowMainMotor.set(TalonFXControlMode.PercentOutput, power);
+        elbowMainMotor.setControl(new DutyCycleOut(power));
     }
 
-    /**
-     * Gets the angle of the shoulder joint.
-     *
-     * @return angle of the shoulder joint. [rad]
-     */
-    public Rotation2d getShoulderJointAngle() {
-        return AngleUtil.normalize(Rotation2d.fromRotations(shoulderEncoder.getAbsolutePosition() - shoulderOffset));
-    }
+
 
     /**
      * Sets the angle of the shoulder joint.
@@ -178,166 +176,173 @@ public class ArmIOReal implements ArmIO {
      *
      * @return elbow joint angle. [rad]
      */
-    public Rotation2d getElbowJointAngle() {
-        return AngleUtil.normalize((Rotation2d.fromRotations(elbowEncoder.getAbsolutePosition() - elbowOffset)));
+//    public Rotation2d getElbowJointAngle() {
+////        return AngleUtil.normalize((Rotation2d.fromRotations(elbowEncoder.getAbsolutePosition() - elbowOffset)));
+//        return (elbowEncoder.getAbsolutePosition().);
+//    }
+    public Rotation2d getShoulderJointAngle() {
+        return Rotation2d.fromRotations(shoulderEncoder.getAbsolutePosition() - shoulderOffset);
     }
 
-    /**
-     * Sets the angle of the elbow joint.
-     *
-     * @param angle desired angle. [degrees]
-     */
-    public void setElbowJointAngle(double angle) {
-        setElbowJointAngle(angle, 0, 0);
-    }
-
-    /**
-     * Sets the angle of the elbow joint.
-     *
-     * @param angle desired angle. [degrees]
-     */
-    public void setElbowJointAngle(double angle, double ffMultiplier, int velocity) {
-        angle = AngleUtil.normalize(angle);
-        Rotation2d error = new Rotation2d(angle).minus(new Rotation2d(currentShoulderAngle));
-        if (elbowEncoder.isConnected()) {
-            motorControlRequest = new PositionVoltage(angle);
-            elbowMainMotor.setControl(new PositionVoltage(angle, velocity, true, shoulderFeedforward * ffMultiplier, 1, true));
-
-        } else {
-            elbowMainMotor.stopMotor();
+        public Rotation2d getElbowJointAngle () {
+            return Rotation2d.fromRotations(elbowEncoder.getAbsolutePosition() - elbowOffset);
         }
-    }
 
-    /**
-     * Calculates the position of the end of the arm.
-     *
-     * @return Translation2d of the position.
-     */
-    public Translation2d getEndPosition() {
-        double shoulderAngle = getShoulderJointAngle().getRadians();
-        double elbowAngle = getElbowJointAngle().getRadians();
-        return kinematics.forwardKinematics(shoulderAngle, shoulderAngle + elbowAngle - Math.PI);
-    }
+            /**
+             * Sets the angle of the elbow joint.
+             *
+             * @param angle desired angle. [degrees]
+             */
+            public void setElbowJointAngle ( double angle){
+                setElbowJointAngle(angle, 0, 0);
+            }
 
-    /**
-     * Sets the position of the end of the arm.
-     *
-     * @param armLocation Translation2d of the desired location.
-     */
-    public void setEndPosition(Translation2d armLocation) {
-        setEndPosition(armLocation, 0, 0);
-    }
+            /**
+             * Sets the angle of the elbow joint.
+             *
+             * @param angle desired angle. [degrees]
+             */
+            public void setElbowJointAngle ( double angle, double ffMultiplier, int velocity){
+                angle = AngleUtil.normalize(angle);
+                Rotation2d error = new Rotation2d(angle).minus(new Rotation2d(currentShoulderAngle));
+                if (elbowEncoder.isConnected()) {
+                    motorControlRequest = new PositionVoltage(angle);
+                    elbowMainMotor.setControl(new PositionVoltage(angle, velocity, true, shoulderFeedforward * ffMultiplier, 1, true));
 
-    /**
-     * Sets the position of the end of the arm.
-     *
-     * @param armLocation Translation2d of the desired location.
-     */
-    public void setEndPosition(Translation2d armLocation, double shoulderFFMultiplier, double elbowFFMultiplier) {
-        var angles = kinematics.inverseKinematics(armLocation);
-        setShoulderJointAngle(Math.toDegrees(angles.shoulderAngle), shoulderFFMultiplier, 0);
-        setElbowJointAngle(Math.toDegrees(angles.elbowAngle), elbowFFMultiplier, 0);
-        ySetpoint = armLocation.getY();
-    }
+                } else {
+                    elbowMainMotor.stopMotor();
+                }
+            }
 
-    /**
-     * Gets the velocity of the shoulder motors.
-     *
-     * @return shoulder motors velocity. [rad/sec]
-     */
-    public double getShoulderMotorVelocity() {
-        return shoulderMainMotor.getVelocity().getValue();
-    }
+            /**
+             * Calculates the position of the end of the arm.
+             *
+             * @return Translation2d of the position.
+             */
+            public Translation2d getEndPosition () {
+                double shoulderAngle = getShoulderJointAngle().getRadians();
+                double elbowAngle = getElbowJointAngle().getRadians();
+                return kinematics.forwardKinematics(shoulderAngle, shoulderAngle + elbowAngle - Math.PI);
+            }
 
-    public void setFinalSetpointAngles(Translation2d position, ArmInputsLogged inputs) {
-        var solution = kinematics.inverseKinematics(position);
-        inputs.finalSetpointAngles[0] = Math.toDegrees(solution.shoulderAngle);
-        inputs.finalSetpointAngles[1] = Math.toDegrees(solution.elbowAngle);
-    }
+            /**
+             * Sets the position of the end of the arm.
+             *
+             * @param armLocation Translation2d of the desired location.
+             */
+            public void setEndPosition (Translation2d armLocation){
+                setEndPosition(armLocation, 0, 0);
+            }
 
-    /**
-     * Gets the velocity of the elbow motors.
-     *
-     * @return elbow motor velocity. [rad/sec]
-     */
-    public double getElbowMotorVelocity() {
-        return elbowMainMotor.getVelocity().getValue();
-    }
+            /**
+             * Sets the position of the end of the arm.
+             *
+             * @param armLocation Translation2d of the desired location.
+             */
+            public void setEndPosition (Translation2d armLocation,double shoulderFFMultiplier, double elbowFFMultiplier)
+            {
+                var angles = kinematics.inverseKinematics(armLocation);
+                setShoulderJointAngle(Math.toDegrees(angles.shoulderAngle), shoulderFFMultiplier, 0);
+                setElbowJointAngle(Math.toDegrees(angles.elbowAngle), elbowFFMultiplier, 0);
+                ySetpoint = armLocation.getY();
+            }
 
-    public void resetArmEncoders() {
-        double elbowAngleReset = elbowEncoder.getAbsolutePosition() * 360.0 - ArmConstants.ELBOW_ZERO_POSITION;
-        double shoulderAngleReset = shoulderEncoder.getAbsolutePosition() * 360.0 - ArmConstants.SHOULDER_ZERO_POSITION;
-        shoulderOffset = AngleUtil.normalize(shoulderAngleReset) / 360.0;
-        elbowOffset = AngleUtil.normalize(elbowAngleReset) / 360.0;
-    }
+            /**
+             * Gets the velocity of the shoulder motors.
+             *
+             * @return shoulder motors velocity. [rad/sec]
+             */
+            public double getShoulderMotorVelocity () {
+                return shoulderMainMotor.getVelocity().getValue();
+            }
 
-    public Translation2d getElbowJointPosition() {
-        Rotation2d shoulderAngle = getShoulderJointAngle();
-        return new Translation2d(
-                ArmConstants.SHOULDER_ARM_LENGTH * shoulderAngle.getCos(),
-                ArmConstants.SHOULDER_ARM_LENGTH * shoulderAngle.getSin());
-    }
+            public void setFinalSetpointAngles (Translation2d position, ArmInputsLogged inputs){
+                var solution = kinematics.inverseKinematics(position);
+                inputs.finalSetpointAngles[0] = Math.toDegrees(solution.shoulderAngle);
+                inputs.finalSetpointAngles[1] = Math.toDegrees(solution.elbowAngle);
+            }
 
-    public boolean armIsOutOfFrame() {
-        Translation2d elbowJoint = getElbowJointPosition(), endPosition = getEndPosition();
-        return !(elbowJoint.getX() < 0) || !(endPosition.getX() < 0);
-    }
+            /**
+             * Gets the velocity of the elbow motors.
+             *
+             * @return elbow motor velocity. [rad/sec]
+             */
+            public double getElbowMotorVelocity () {
+                return elbowMainMotor.getVelocity().getValue();
+            }
 
-    public boolean armIsInRobot() {
-        return getEndPosition().getY() < 0 && getShoulderJointAngle().getDegrees() > 90;
-    }
+            public void resetArmEncoders () {
+                double elbowAngleReset = elbowEncoder.getAbsolutePosition() * 360.0 - ArmConstants.ELBOW_ZERO_POSITION;
+                double shoulderAngleReset = shoulderEncoder.getAbsolutePosition() * 360.0 - ArmConstants.SHOULDER_ZERO_POSITION;
+                shoulderOffset = AngleUtil.normalize(shoulderAngleReset) / 360.0;
+                elbowOffset = AngleUtil.normalize(elbowAngleReset) / 360.0;
+            }
 
-    public void setVelocity(Translation2d velocity) {
-        var armVelocities = kinematics.getVelocities(getEndPosition(), velocity);
-        setShoulderJointPower(armVelocities.shoulderAngle / Math.PI);
-        setElbowJointPower(armVelocities.elbowAngle / (2 * Math.PI));
-    }
+            public Translation2d getElbowJointPosition () {
+                Rotation2d shoulderAngle = getShoulderJointAngle();
+                return new Translation2d(
+                        ArmConstants.SHOULDER_ARM_LENGTH * shoulderAngle.getCos(),
+                        ArmConstants.SHOULDER_ARM_LENGTH * shoulderAngle.getSin());
+            }
 
-    public boolean changedToDefaultCommand() {
-        return changedToDefaultCommand;
-    }
+            public boolean armIsOutOfFrame () {
+                Translation2d elbowJoint = getElbowJointPosition(), endPosition = getEndPosition();
+                return !(elbowJoint.getX() < 0) || !(endPosition.getX() < 0);
+            }
 
-    /**
-     * Stops the motors.
-     */
-    public void stop() {
-        shoulderMainMotor.set(0);
-        elbowMainMotor.set(0);
-    }
+            public boolean armIsInRobot () {
+                return getEndPosition().getY() < 0 && getShoulderJointAngle().getDegrees() > 90;
+            }
 
-    @Override
-    public String getSubsystemName() {
-        return "Arm";
-    }
+            public void setVelocity (Translation2d velocity){
+                var armVelocities = kinematics.getVelocities(getEndPosition(), velocity);
+                setShoulderJointPower(armVelocities.shoulderAngle / Math.PI);
+                setElbowJointPower(armVelocities.elbowAngle / (2 * Math.PI));
+            }
+
+            public boolean changedToDefaultCommand () {
+                return changedToDefaultCommand;
+            }
+
+            /**
+             * Stops the motors.
+             */
+            public void stop () {
+                shoulderMainMotor.set(0);
+                elbowMainMotor.set(0);
+            }
+
+            @Override
+            public String getSubsystemName () {
+                return "Arm";
+            }
 
 
+            @Override
+            public void updateInputs (ArmInputsLogged inputs){
+                inputs.shoulderAngle = getShoulderJointAngle().getDegrees();
+                inputs.elbowAngle = getElbowJointAngle().getDegrees();
+                inputs.shoulderMotorPower = getShoulderJointPower();
+                inputs.elbowMotorPower = getElbowJointPower();
+                inputs.shoulderEncoderPosition = shoulderEncoder.getAbsolutePosition();
+                inputs.elbowEncoderPosition = elbowEncoder.getAbsolutePosition();
+                inputs.shoulderVelocity = getShoulderMotorVelocity();
+                inputs.elbowVelocity = getElbowMotorVelocity();
+                Translation2d position = getEndPosition();
+                inputs.armPosition[0] = position.getX();
+                inputs.armPosition[1] = position.getY();
+                ArmKinematics.InverseKinematicsSolution kinematicsSolution = kinematics.inverseKinematics(position);
+                inputs.inverseKinematicsSolution[0] = Math.toDegrees(kinematicsSolution.shoulderAngle);
+                inputs.inverseKinematicsSolution[1] = Math.toDegrees(kinematicsSolution.elbowAngle);
+                inputs.ySetpoint = ySetpoint;
 
+                inputs.feedforward[0] = shoulderFeedforward;
+                inputs.feedforward[1] = elbowFeedforward;
 
-    @Override
-    public void updateInputs(ArmInputsLogged inputs) {
-        inputs.shoulderAngle = getShoulderJointAngle().getDegrees();
-        inputs.elbowAngle = getElbowJointAngle().getDegrees();
-        inputs.shoulderMotorPower = shoulderMainMotor.getMotorOutputPercent();
-        inputs.elbowMotorPower = elbowMainMotor.getMotorOutputPercent();
-        inputs.shoulderEncoderPosition = shoulderEncoder.getAbsolutePosition();
-        inputs.elbowEncoderPosition = elbowEncoder.getAbsolutePosition();
-        inputs.shoulderVelocity = getShoulderMotorVelocity();
-        inputs.elbowVelocity = getElbowMotorVelocity();
-        Translation2d position = getEndPosition();
-        inputs.armPosition[0] = position.getX();
-        inputs.armPosition[1] = position.getY();
-        ArmKinematics.InverseKinematicsSolution kinematicsSolution = kinematics.inverseKinematics(position);
-        inputs.inverseKinematicsSolution[0] = Math.toDegrees(kinematicsSolution.shoulderAngle);
-        inputs.inverseKinematicsSolution[1] = Math.toDegrees(kinematicsSolution.elbowAngle);
-        inputs.ySetpoint = ySetpoint;
+            }
 
-        inputs.feedforward[0] = shoulderFeedforward;
-        inputs.feedforward[1] = elbowFeedforward;
+            public ArmKinematics getKinematics () {
+                return kinematics;
+            }
+        }
 
-        inputs.shoulderOutputVoltage = shoulderMainMotor.getMotorOutputVoltage();
-    }
-
-    public ArmKinematics getKinematics() {
-        return kinematics;
-    }
-}
