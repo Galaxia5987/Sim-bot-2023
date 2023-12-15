edcb9a8769def8698fcfc343080bb181035dd1e1
GaiaZano05 <gaya.zano11@gmail.com>
2023-12-15 10:36:25 +0200

    delete AutoLog

--- src/main/java/frc/robot/subsystems/arm/ArmIO.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIO.java
@@ -33,23 +33,4 @@ public interface ArmIO {
         PRECENT_OUTPUT,
         POSITION
     }
-
-    @AutoLog
-    class ArmInputs {
-
-        public double shoulderAngleSetPoint = 0;
-        double shoulderAppliedVoltage = 0;
-        double elbowAppliedVoltage = 0;
-        double shoulderAppliedCurrent = 0;
-        double elbowAppliedCurrent = 0;
-        double[] shoulderTipPose = new double[2];
-        double[] endEffectorPositionSetPoint = new double[2];
-        double shoulderAngle = 0;
-        double elbowAngleSetpoint = 0;
-        double elbowAngleRelative = 0;
-        double elbowAngleAbsolute = 0;
-        Translation2d endEffectorPose;
-        ControlMode shoulderControlMode = ControlMode.PRECENT_OUTPUT;
-        ControlMode elbowControlMode = ControlMode.PRECENT_OUTPUT;
-    }
 }
